cc20fa4b2b469710ae17cf2a68bd9076f36cfcf7
schto223 <scht223@gmail.com>
2024-05-22 17:54:57 +0200

    updated for publishing

--- docs/make.jl
+++ docs/make.jl
@@ -30,21 +30,6 @@ makedocs(
     checkdocs = :exports # remove later or replace by :exports
 )
 
-"""makedocs(
-    modules = [FlipGraphs],
-    format = Documenter.HTML(
-        # Use clean URLs, unless built as a "local" build
-        prettyurls = !("local" in ARGS),
-        #canonical = "https://juliadocs.github.io/Documenter.jl/stable/",
-    ),
-    sitename = "FlipGraphs Documentation",
-#    authors = "Tom Schmit",
-    pages = [
-        "About" => "index.md"
-    ]
-)
-
 deploydocs(
     repo = "github.com/schto223/FlipGraphs.jl.git",
 )
-"""
