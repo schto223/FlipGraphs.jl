968e342531b8ab82cbbb078e9a6aa642578e6404
schto223 <scht223@gmail.com>
2024-06-26 21:31:35 +0200

    fixed exporting test

--- test/exporting.jl
+++ test/exporting.jl
@@ -1,12 +1,12 @@
 @testset "exporting" begin
     D = deltacomplex(2)
     G = flipgraph_modular(D)
-    export_gml("flipG_test.gml", G, :diameter)
+    export_gml("flipG_test.gml", G, add_diameter=true)
     @test isfile("flipG_test.gml")==true
     rm("flipG_test.gml")
 
     G2 = flipgraph_planar(8)
-    export_gml("flipG2_test", G2, :diameter)
+    export_gml("flipG2_test", G2, add_diameter=true)
     @test isfile("flipG2_test.gml")==true
     rm("flipG2_test.gml")
 end
